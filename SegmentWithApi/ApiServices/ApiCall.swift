4cc27010917fa6b83ceccbbb34f02566d3bc245a
hirenmasaliya <112078959+hirenmasaliya@users.noreply.github.com>
2024-12-07 18:19:18 +0530

    Segment Controller using Api Data Fetch or show and CoreData

---
+++ SegmentWithApi/ApiServices/ApiCall.swift
@@ -0,0 +1,8 @@
+//
+//  ApiCall.swift
+//  SegmentWithApi
+//
+//  Created by Hiren Masaliya on 03/12/24.
+//
+
+import Foundation
---
+++ SegmentWithApi/Model/JokeModel.swift
@@ -0,0 +1,8 @@
+//
+//  JokeModel.swift
+//  SegmentWithApi
+//
+//  Created by Hiren Masaliya on 03/12/24.
+//
+
+import Foundation
